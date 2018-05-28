dd2a4b1114ac7cd242b8ecf4c7819df54c9fcb86
cycateng <cyber.gcw@gmail.com>
2018-05-28 13:01:23 +0900

    実行結果を見やすく改造

--- hashtest.py
+++ hashtest.py
@@ -1,6 +1,18 @@
 from hashlib import sha256
 x = 5
 y = 0 # We don't know what y should be yet...
-while sha256(f"{x*y}".encode()).hexdigest()[-1] != "0":
+h = sha256(f'{x*y}'.encode()).hexdigest()
+print('calc y(nonce)...', end='\n\n')
+while h[-1] != "0":
+    print(f'y = {y}:\thash = {h}')
     y += 1
-    print(f'The solution is y = {y}')
+    h = sha256(f'{x*y}'.encode()).hexdigest()
+
+print(f'y = {y}:\thash = {h}')
+print(f'The solution is y = {y}')
+
+#y = 0
+#while sha256(f'{x*y}'.encode()).hexdigest()[-1] != "0":
+#    y += 1
+#    print(f'The solution is y = {y}')
+
